9470b3c53c850e4e103554de02ed1505104ced55
Norbel AMBANUMBEN <aanorbel@gmail.com>
2018-01-08 19:31:01 +0100

    completed BMI

--- BodyMassIndexMobile/src/main/java/com/aanorbel/example/bodymassindexmobile/MainActivity.java
+++ BodyMassIndexMobile/src/main/java/com/aanorbel/example/bodymassindexmobile/MainActivity.java
@@ -18,9 +18,18 @@ public class MainActivity extends AppCompatActivity {
 
         mWeightEditText = findViewById(R.id.weight);
         mHeightEditText = findViewById(R.id.height);
+        mBMITextView = findViewById(R.id.bmi);
     }
 
     public void calculateBodyMassIndex(View view) {
+
+        mBMITextView.setText(String.valueOf(getDoubleFromEditText(mWeightEditText) / (Math.pow(getDoubleFromEditText(mHeightEditText), 2))));
+
+    }
+
+    private double getDoubleFromEditText(EditText editText) {
+
+        return Double.valueOf(((editText.getText().length() > 0) ? (editText).getText().toString() : 0).toString());
 
     }
 }
