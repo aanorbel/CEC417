e2e5c597a6237d1d4817ba796ea54028704fddd9
Norbel AMBANUMBEN <aanorbel@gmail.com>
2018-01-08 08:30:12 +0100

    added leap year program

---
+++ LeapYear/src/main/java/com/aanorbel/example/leapyear/LeapYearTester.java
@@ -0,0 +1,24 @@
+package com.aanorbel.example.leapyear;
+
+public class LeapYearTester {
+    public static void main(String[] args) {
+
+        System.out.println("All Leap Years Between 2001 and 2100");
+
+        int count = 0;
+
+        for (int year = 2001; year <= 2100; year++) {
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+
+                count++;
+                System.out.print(year + (count % 10 == 0 ? "\n" : " "));
+
+            }
+        }
+
+        System.out.println();
+
+    }
+
+}
